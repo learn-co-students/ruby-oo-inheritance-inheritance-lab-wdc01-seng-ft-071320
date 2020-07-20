ebf17060dc5289c20e020abb20f890564a0be1e5
acparkgh <acpark123@gmail.com>
2020-07-20 03:46:43 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    new
+  end
+
+  def new
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  # def knowledge
+  #   @knowledge
+  # end
 
 end
